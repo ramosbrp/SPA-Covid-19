ed2a5144a66287a852ef8a932932c20b08d3a8b3
Bruno <bruno.rp45@gmail.com>
2021-07-13 19:54:22 -0300

    ngFor funcionando nas noticias

--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -1,5 +1,11 @@
-export interface Post {
-    "title": string,
-    "body": string,
-    "id": number
+export class Post {
+    public "title": string
+    public "body": string
+    public "id": number
+
+    constructor(title: string, body: string, id: number){
+        this.title = title
+        this.body = body
+        this.id = id
+    }
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,6 +9,7 @@ import { Post } from 'src/app/model/post';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  list=[0,1,2,3,4,5,6,7]
   title = ''  
   text = ''
   
